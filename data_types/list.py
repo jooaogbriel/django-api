9e4f78b9991f446a725ffdc51ff007d6e3f1f3e5
João Gonçalves <joao_i0807@outlook.com>
2023-11-17 16:53:04 -0300

    feat try for in list

--- data_types/list.py
+++ data_types/list.py
@@ -1,5 +1,9 @@
 my_list = [1, 'oi', 2, 'tudo bem', [1.33]]
 
 def list_fundamentals():
-    for item in my_list:
-        print(item)
+    # for item in my_list:
+    #     print(item)
+    
+    # SEARCH BY INDEX WITH FOR 
+    for item_by_index in enumerate(my_list):
+        print(item_by_index[1])
