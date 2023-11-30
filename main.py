d58b9061b23fe56f0d1734fe4e20877d08f644f7
João Gonçalves <joao_i0807@outlook.com>
2023-11-30 14:36:49 -0300

    feat: example example of inserting data into the database with django

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from residencias.classes import Residencia, Apartamento, Casa
-
+# from class_residencias.classes import Residencia, Apartamento, Casa
+class Person():
+    ...
 def main():
-    casa_1 = Casa(6, 33.5, "Rua Francisca",True)
-    print(casa_1.alugado_em)
-    casa_1.alugar()
-    print(casa_1.alugado_em)
-    print(casa_1)
+    # casa_1 = Casa(6, 33.5, "Rua Francisca",True)
+    # print(casa_1.alugado_em)
+    # casa_1.alugar()
+    # print(casa_1.alugado_em)
+    # print(casa_1)
+    ...
 
 if __name__ == "__main__":
     main()
 
+#FORMA DE COLOCAR UM DICIONÁRIO DENTRO DO BANCO
+person_data = {'name': 'João'}
+joao = Person(**person_data)
+joao.save()
 
 
