75c1f1880f3cffab04318bccece91e024e390dab
João Gonçalves <joao_i0807@outlook.com>
2023-12-01 16:49:28 -0300

    feat: run server

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Article (models.Model):
+class Article(models.Model):
     title = models.TextField()
     content = models.TextField()
+
+class Account (models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField(null=True)
+    is_married = models.BooleanField(default=False)
+    
+def __repr__(self) -> str:
+    return f"{self.title} - {self.content}"
--- trydjango/views.py
+++ trydjango/views.py
@@ -3,7 +3,7 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
-from django.template.loader import render_to_string
+
 from articles.models import Article
 
 
@@ -13,17 +13,14 @@ def home_view(request):
     Return HTML as a response (We pick to return the response)
     """
     # from the database??
-    article_obj = Article.objects.get()
+    article_obj = Article.objects.get(title='try python')
 
-    context = {
-        "object": article_obj,
-        "title": article_obj.title,
-        "content": article_obj.content
-    }
     # Django Templates
-    HTML_STRING = render_to_string("home-view.html", context=context)
-    # HTML_STRING = """
-    # <h1>{title} (id: {id})!</h1>
-    # <p>{content}!</p>
-    # """.format(**context)
+    H1_STRING = f"""
+    <h1>{article_obj.title}
+    """
+    P_STRING = f"""
+    <p>{article_obj.content}!</p>
+    """
+    HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
