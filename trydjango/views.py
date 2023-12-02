5924052111007b5f4baf855952b11c9f1fae27c6
João Gonçalves <joao_i0807@outlook.com>
2023-12-02 09:34:11 -0300

    fix: correcting it to use my app view and not the projects view

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,26 +1,26 @@
-"""
-To render html web pages
-"""
-import random
-from django.http import HttpResponse
+# """
+# To render html web pages
+# """
+# import random
+# from django.http import HttpResponse
 
-from articles.models import Article
+# from articles.models import Article
 
 
-def home_view(request):
-    """
-    Take in a request (Django sends request)
-    Return HTML as a response (We pick to return the response)
-    """
-    # from the database??
-    article_obj = Article.objects.get(title='try python')
+# def home_view(request):
+#     """
+#     Take in a request (Django sends request)
+#     Return HTML as a response (We pick to return the response)
+#     """
+#     # from the database??
+#     article_obj = Article.objects.get(title='try python')
 
-    # Django Templates
-    H1_STRING = f"""
-    <h1>{article_obj.title}
-    """
-    P_STRING = f"""
-    <p>{article_obj.content}!</p>
-    """
-    HTML_STRING = H1_STRING + P_STRING
-    return HttpResponse(HTML_STRING)
+#     # Django Templates
+#     H1_STRING = f"""
+#     <h1>{article_obj.title}
+#     """
+#     P_STRING = f"""
+#     <p>{article_obj.content}!</p>
+#     """
+#     HTML_STRING = H1_STRING + P_STRING
+#     return HttpResponse(HTML_STRING)
