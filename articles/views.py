b51db5963d403de4f2d1d76b4618b6026eecd02c
João Gonçalves <joao_i0807@outlook.com>
2023-12-04 20:12:15 -0300

    feat: start project in develop branch

--- articles/views.py
+++ articles/views.py
@@ -3,3 +3,14 @@ from rest_framework.views import Request, Response, APIView, status
 class ArticlesViews(APIView):
     def post(self, req: Request) -> Response:
         return Response({"msg: post criado"}, status.HTTP_201_CREATED)
+    
+from django.shortcuts import render
+from .models import BlogPost, Comment
+
+def blog(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog.html', {'posts': posts})
+
+def about_me(request):
+    # Adicione informações sobre você e seus projetos aqui
+    return render(request, 'about_me.html')
